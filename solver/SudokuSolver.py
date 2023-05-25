049de103ab6c8959104489a7f3ec1707581216a4
George <george-andreas@hotmail.com>
2023-05-24 21:56:04 -0700

    added type annotation on sudoku solver

--- solver/SudokuSolver.py
+++ solver/SudokuSolver.py
@@ -5,14 +5,14 @@ class SudokuSolver:
     """
         Sudoku Solver class using backtracking and a custom pre-solver
     """
-    def __init__(self, board):
+    def __init__(self, board: list[list[int]]):
         self.board = board
     
     def solve(self):
         pre_sudoku_solver = SudokuPreSolver(self.board)
         pre_sudoku_solver.solve()
 
-        sudoku_backtrack = SudokuBacktrack(pre_sudoku_solver.board)
+        sudoku_backtrack = SudokuBacktrack(self.board)
         sudoku_backtrack.solve()
 
         
